dde926f82089311fe65667d5aee96a034990669e
DiscountHub <k.proj.2019@gmail.com>
2025-08-01 14:46:26 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,17 @@
-// pages/_app.js
-import { useEffect } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import '../global.css'; // если у вас есть стили, подключите их
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  useEffect(() => {
-    // можно вставить логику инициализации (например, аналитика)
-  }, [router.pathname]);
-
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
         <title>DiscountHub</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Component {...pageProps} />
+      <Component key={router.route} {...pageProps} />
     </>
   );
 }
